7c1d4c76c96e18eeb44ebd53d62516aff7c0b55b
Ralph <rpbarczok@gmx.de>
2025-06-27 16:43:12 +0200

    add bonus expert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,12 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 //Schreibe ein Programm, das den Namen des Nutzers einliest (Scanner) und ihn mit Namen begrüßt.
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {6,2,8,3,9,6};
-        sortArray(numbers);
-        System.out.println(Arrays.toString(sortArray(numbers)));
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(romanNumber(i));
+        }
     }
 
     public static String reverseString(String str){
@@ -36,6 +33,65 @@ public class Main {
         return sortedArray;
     }
 
+    public static String[] stringToArray (String str) {
+        return str.split(",");
+    }
 
+    public static int quersumme (int number) {
+        if  (number > 0) {
+            number = Math.abs(number);
+        }
+        if (number < 10) {
+            return number;
+        }
+        String numberString = String.valueOf(number);
+        String[] numberStringArray =  numberString.split("");
+        int sum = 0;
+        for (String str : numberStringArray) {
+            sum = sum + Integer.parseInt(str);
+        }
+        return sum;
+    }
 
+    public static String romanNumber (int number) {
+        if (number > 100 || number < 1) {
+            return "Gib eine Zahl zwischen 1 und 100 an!";
+        }
+        if (number == 100) {
+            return "C";
+        }
+        int tens = number / 10;
+        int ones = number % 10;
+        return (romanNumberTens(tens)) + (romanNumberOnes(ones));
+    }
+
+    public static String romanNumberOnes(int number) {
+        return switch(number) {
+            case 1 -> "I";
+            case 2 -> "II";
+            case 3 -> "III";
+            case 4 -> "IV";
+            case 5 -> "V";
+            case 6 -> "VI";
+            case 7 -> "VII";
+            case 8 -> "VIII";
+            case 9 -> "IX";
+            default -> "";
+        };
+    }
+
+    public static String romanNumberTens(int number) {
+        return switch(number) {
+            case 1 -> "X";
+            case 2 -> "XX";
+            case 3 -> "XXX";
+            case 4 -> "XL";
+            case 5 -> "L";
+            case 6 -> "LX";
+            case 7 -> "LXX";
+            case 8 -> "LXXX";
+            case 9 -> "XC";
+            default -> "";
+        };
+    }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,14 +1,50 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class MainTest {
+public class MainTest {
 
     @Test
-    void reverseString() {
+    void reverseString_shouldReturnollaH_whenCalledWithHallo() {
+        String result = Main.reverseString("Hallo");
+        assertEquals("ollaH", result);
     }
 
     @Test
-    void sortArray() {
+    void sortArray_shouldReturnsSortedArray_whenCalledWithIntArray() {
+        int[] input = {6, 2, 8, 3, 9, 6};
+        int[] expected = {2, 3, 6, 6, 8, 9};
+
+        int[] actual = Main.sortArray(input);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void stringToArray_shouldReturnArray_whenCalledWithString() {
+        String input = "eins,zwei,drei,vier,fünf";
+        String[] expected = {"eins", "zwei", "drei","vier", "fünf"};
+
+        String[] actual = Main.stringToArray(input);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void quersumme_shouldReturn12_whenCalledWith732() {
+        int input=732;
+        int expected=12;
+        int actual=Main.quersumme(input);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void romanNumber_shouldReturnLXXIV_whenCalledWith74() {
+        int input=74;
+        String expected="LXXIV";
+
+        assertEquals(expected,Main.romanNumber(input));
     }
 }
