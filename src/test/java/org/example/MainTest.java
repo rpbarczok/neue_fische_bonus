4b287de5d5d3d3dea8c88cb0a7defc5b60973a3d
Ralph <rpbarczok@gmx.de>
2025-07-01 08:53:04 +0200

    new tests

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -41,6 +41,21 @@ public class MainTest {
     }
 
     @Test
+    void romanNumber_shouldReturnIII_whenCalledWith3() {
+        int input=3;
+        String expected="III";
+
+        assertEquals(expected,Main.romanNumber(input));
+    }
+
+    void romanNumber_shouldReturnC_whenCalledWith100() {
+        int input=100;
+        String expected="C";
+
+        assertEquals(expected,Main.romanNumber(input));
+    }
+
+    @Test
     void romanNumber_shouldReturnLXXIV_whenCalledWith74() {
         int input=74;
         String expected="LXXIV";
